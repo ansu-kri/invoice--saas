f228022870f1f8a2d747e884df9a9c55f42928f0
ansu-kri <ansukummari@gmail.com>
2026-03-10 20:57:03 +0530

    fix smtp port to 587

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -35,8 +35,8 @@ exports.sendInvoiceEmail = async (invoice) => {
 
           const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
               user: process.env.EMAIL_USER,
               pass: process.env.EMAIL_PASS,
