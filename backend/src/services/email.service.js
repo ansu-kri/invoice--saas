87f33e83d6bbfde66c4e7e78834fd27b977a7098
ansu-kri <ansukummari@gmail.com>
2026-03-10 21:14:43 +0530

    fix smtp ipv4

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -37,9 +37,13 @@ exports.sendInvoiceEmail = async (invoice) => {
             host: "smtp.gmail.com",
             port: 587,
             secure: false,
+            family: 4, // force IPv4 (important)
             auth: {
               user: process.env.EMAIL_USER,
               pass: process.env.EMAIL_PASS,
+            },
+            tls: {
+              rejectUnauthorized: false,
             },
           });
 
